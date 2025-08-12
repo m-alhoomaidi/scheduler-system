49214547e239f4f154ae26148430f010e201b0b3
Mohammed Abdo <=m.alhoomaidi@gmail.com>
2025-08-12 05:01:43 +0300

    fix: fix spring test

--- apps/scheduler-engine/src/test/java/com/scheduler/scheduler_engine/SchedulerEngineApplicationTests.java
+++ apps/scheduler-engine/src/test/java/com/scheduler/scheduler_engine/SchedulerEngineApplicationTests.java
@@ -2,8 +2,10 @@ package com.scheduler.scheduler_engine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SchedulerEngineApplicationTests {
 
 	@Test
